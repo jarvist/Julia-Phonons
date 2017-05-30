da8592075b03d1544cbeabb49ead7feda660b1e4
Jarvist Moore Frost <jarvist@gmail.com>
2017-05-30 17:36:38 +0100

    Starting to work up codes.

---
+++ 2017-05-ShakeRattleRoll/ShakeRattleRoll.jl
@@ -0,0 +1,28 @@
+# phonopy_project.jl
+# Demonstration / scratchpad for using JuliaPhonons module
+
+push!(LOAD_PATH,"../src") # Temporary versions of modules in PWD
+using JuliaPhonons 
+
+# The REPO comes with a basic MAPI cubic unit cell Phonopy calculation
+P=read_POSCAR(open("../2015-11_CubicModeDecomposition/MAPbI/POSCAR"),expansion=[2,2,2])
+eigenvectors,eigenmodes=read_meshyaml(open("../2015-11_CubicModeDecomposition/MAPbI/mesh.yaml"),P)
+
+myf=open("mode_decomposition.dat","w")
+gnuplot_header(P,f=myf)
+
+show(eigenvectors[16])
+
+end
+
+for (count,(eigenvector,eigenmode)) in enumerate(zip(eigenvectors,eigenmodes))
+    output_animated_xyz(P,count,eigenvector,eigenmode) # generates files anim_{count}.xyz
+ 
+    decompose_eigenmode_atomtype(P,count,eigenvector,eigenmode)
+    # Output again, to a file for later printing
+    decompose_eigenmode_atomtype(P,count,eigenvector,eigenmode,f=myf)
+
+    decompose_eigenmode_atom_contributions(P,count,eigenvector)
+end
+
+close(myf)
